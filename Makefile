ab0ca060fd3176954b38753f3b068b414f08ff2e
Dahuum <romaissaeabde@gmail.com>
2025-06-22 19:07:16 +0100

    h

---
+++ Makefile
@@ -0,0 +1,26 @@
+FLAGS = -g -Wall -Wextra -Werror 
+SRC = minishell.c ./utils/split.c ./utils/ft_libft.c \
+		./utils/errors.c
+
+OBJ = $(SRC:.c=.o)
+NAME = minishell
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	@cc $(FLAGS) $(OBJ) -o $(NAME) -lreadline
+
+%.o: %.c
+	@cc $(FLAGS) -c $< -o $@
+
+clean:
+	@rm -rf $(OBJ) $(OBJ_BONUS)
+	@rm -rf *.o
+
+fclean: clean
+	@rm -rf $(NAME) $(BONUS_NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re bonus
+.SECONDARY: $(OBJ)
---
+++ minishell.h
@@ -0,0 +1,38 @@
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <limits.h>
+#include <termios.h>
+#include <curses.h>
+#include <term.h>
+#include <sys/ioctl.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+/* functions of lib_ft utils*/
+char *_strcat(char *dest, char *src);
+char	**ft_split(char const *s, char c);
+
+/* Split Utils */
+int	count_strings(char const *s, char c);
+char	*malloc_strings(const char *s, char c);
+
+/* Free Utils */
+void	free_split(char **split);
+
+/* Error Handling functions */
+void cmd_not_found(char *cmd, char **s, char *line, char *path);
+
+#endif // MINI_SHELL_H
